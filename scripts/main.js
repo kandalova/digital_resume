af5a42ee0d8d64ca7e90f5b1decd7274a3a03d34
Leyla Kandalova <leyla_kandalova@epam.com>
2023-02-18 01:53:45 +0600

    Fixes for deploy

--- scripts/main.js
+++ scripts/main.js
@@ -25,34 +25,27 @@
 
 class CollapsedBlock extends HTMLElement {
 	connectedCallback(){
-		$.get('/components/CollapsedBlock/CollapsedBlockTemplate.html', function(html) {
-			// Convert the HTML string to a DOM object
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(html, 'text/html');
+		const template = document.querySelector('#collapsed-block-id');
 
-			// Get the template element
-			const template = doc.querySelector('#collapsed-block-id');
+		// Create a shadow DOM for the web component
+		const shadow = this.attachShadow({ mode: 'open' });
 
-			// Create a shadow DOM for the web component
-			const shadow = this.attachShadow({ mode: 'open' });
+		// Clone the template content and append it to the shadow DOM
+		const instance = template.content.cloneNode(true);
+		shadow.appendChild(instance);
 
-			// Clone the template content and append it to the shadow DOM
-			const instance = template.content.cloneNode(true);
-			shadow.appendChild(instance);
-
-			const header = this.shadowRoot.querySelector('.block-switch--header');
-		
-			header.onclick = () => {
-				header.classList.toggle('active');
-				const body = this.shadowRoot.querySelector('.block-switch--body');
-				if (body.style.maxHeight){
-					body.style.maxHeight = null;
-				} else {
-					body.style.maxHeight = body.scrollHeight + "px";
-				}
-			};
-		}.bind(this));
-}
+		const header = this.shadowRoot.querySelector('.block-switch--header');
+	
+		header.onclick = () => {
+			header.classList.toggle('active');
+			const body = this.shadowRoot.querySelector('.block-switch--body');
+			if (body.style.maxHeight){
+				body.style.maxHeight = null;
+			} else {
+				body.style.maxHeight = body.scrollHeight + "px";
+			}
+		};
+	}
 }
 
 customElements.define("collapsed-block", CollapsedBlock);
