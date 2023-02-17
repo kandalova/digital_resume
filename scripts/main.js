225b6c32bb9b732b0e6c5be129aace7e33a4cbcc
Leyla Kandalova <leyla_kandalova@epam.com>
2023-02-17 19:36:37 +0600

    Fix texts and add collapsed projects

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const blockSwitches = document.getElementsByClassName("block-switch--header");
+
+for (let i = 0; i < blockSwitches.length; i++){
+	blockSwitches[i].addEventListener("click", function(){
+		this.classList.toggle("active");
+		const body = this.nextElementSibling;
+    if (body.style.maxHeight){
+      body.style.maxHeight = null;
+    } else {
+      body.style.maxHeight = body.scrollHeight + "px";
+    }
+	})
+}
+
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'pages/' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })
+})
