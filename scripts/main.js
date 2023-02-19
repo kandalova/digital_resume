e3cc415cc9ca90ad237db65f92780dc3ee8ce48e
Leyla Kandalova <leyla_kandalova@epam.com>
2023-02-19 20:12:53 +0600

    More fixes

--- scripts/main.js
+++ scripts/main.js
@@ -14,13 +14,13 @@ for (let i = 0; i < blockSwitches.length; i++){
 	})
 }
 
-$(function () {
-  var includes = $('[data-include]')
-  $.each(includes, function () {
-    var file = 'pages/' + $(this).data('include') + '.html'
-    $(this).load(file)
-  })
-})
+// $(function () {
+//   var includes = $('[data-include]')
+//   $.each(includes, function () {
+//     var file = 'pages/' + $(this).data('include') + '.html'
+//     $(this).load(file)
+//   })
+// })
 
 
 
